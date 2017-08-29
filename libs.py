08e6f8456129e544bd5692e9577a7858285d2d7c
thelumberzach <zacharytimberlake@gmail.com>
2017-08-28 19:31:18 -0700

    for sam

--- libs.py
+++ libs.py
@@ -77,7 +77,7 @@ Pokedex = {
         'Dragon': 1,
         'Dark': 2,
         'Fairy': .5
-    }
+    },
     'Flying': {
         'Normal': 1,
         'Fighting': 2,
@@ -97,7 +97,7 @@ Pokedex = {
         'Dragon': 1,
         'Dark': 1,
         'Fairy': 1
-    }
+    },
     'Poison': {
         'Normal': 1,
         'Fighting': 1,
@@ -117,7 +117,7 @@ Pokedex = {
         'Dragon': 1,
         'Dark': 1,
         'Fairy': 2
-    }
+    },
     'Ground': {
         'Normal': 1,
         'Fighting': 1,
@@ -137,7 +137,7 @@ Pokedex = {
         'Dragon': 1,
         'Dark': 1,
         'Fairy': 1
-    }
+    },
     'Rock': {
         'Normal': 1,
         'Fighting': .5,
@@ -157,7 +157,7 @@ Pokedex = {
         'Dragon': 1,
         'Dark': 1,
         'Fairy': 1
-    }
+    },
     'Bug': {
         'Normal': 1,
         'Fighting': .5,
@@ -177,7 +177,7 @@ Pokedex = {
         'Dragon': 1,
         'Dark': 2,
         'Fairy': .5
-    }
+    },
     'Ghost': {
         'Normal': 0,
         'Fighting': 1,
@@ -197,7 +197,7 @@ Pokedex = {
         'Dragon': 1,
         'Dark': .5,
         'Fairy': 1
-    }
+    },
     'Steel': {
         'Normal': 1,
         'Fighting': 1,
@@ -217,7 +217,7 @@ Pokedex = {
         'Dragon': 1,
         'Dark': 1,
         'Fairy': 2
-    }
+    },
     'Fire': {
         'Normal': 1,
         'Fighting': 1,
@@ -237,7 +237,7 @@ Pokedex = {
         'Dragon': .5,
         'Dark': 1,
         'Fairy': 1
-    }
+    },
     'Water': {
         'Normal': 1,
         'Fighting': 1,
@@ -257,7 +257,7 @@ Pokedex = {
         'Dragon': .5,
         'Dark': 1,
         'Fairy': 1
-    }
+    },
     'Grass': {
         'Normal': 1,
         'Fighting': 1,
@@ -275,127 +275,127 @@ Pokedex = {
         'Psychic': 1,
         'Ice': 1,
         'Dragon': .5,
-        'Dark': 2,
-        'Fairy': .5
-    }
+        'Dark': 1,
+        'Fairy': 1
+    },
     'Electric': {
-        'Normal': 2,
+        'Normal': 1,
         'Fighting': 1,
-        'Flying': .5,
-        'Poison': .5,
-        'Ground': 1,
-        'Rock': 2,
-        'Bug': .5,
-        'Ghost': 0,
-        'Steel': 2,
+        'Flying': 2,
+        'Poison': 1,
+        'Ground': 0,
+        'Rock': 1,
+        'Bug': 1,
+        'Ghost': 1,
+        'Steel': 1,
         'Fire': 1,
-        'Water': 1,
-        'Grass': 1,
-        'Electric': 1,
-        'Psychic': .5,
-        'Ice': 2,
-        'Dragon': 1,
-        'Dark': 2,
-        'Fairy': .5
-    }
+        'Water': 2,
+        'Grass': .5,
+        'Electric': .5,
+        'Psychic': 1,
+        'Ice': 1,
+        'Dragon': .5,
+        'Dark': 1,
+        'Fairy': 1
+    },
     'Psychic': {
-        'Normal': 2,
-        'Fighting': 1,
-        'Flying': .5,
-        'Poison': .5,
+        'Normal': 1,
+        'Fighting': 2,
+        'Flying': 1,
+        'Poison': 2,
         'Ground': 1,
-        'Rock': 2,
-        'Bug': .5,
-        'Ghost': 0,
-        'Steel': 2,
+        'Rock': 1,
+        'Bug': 1,
+        'Ghost': 1,
+        'Steel': .5,
         'Fire': 1,
         'Water': 1,
         'Grass': 1,
         'Electric': 1,
         'Psychic': .5,
-        'Ice': 2,
+        'Ice': 1,
         'Dragon': 1,
-        'Dark': 2,
-        'Fairy': .5
-    }
+        'Dark': 0,
+        'Fairy': 1
+    },
     'Ice': {
-        'Normal': 2,
+        'Normal': 1,
         'Fighting': 1,
-        'Flying': .5,
-        'Poison': .5,
-        'Ground': 1,
-        'Rock': 2,
-        'Bug': .5,
-        'Ghost': 0,
-        'Steel': 2,
-        'Fire': 1,
-        'Water': 1,
-        'Grass': 1,
+        'Flying': 2,
+        'Poison': 1,
+        'Ground': 2,
+        'Rock': 1,
+        'Bug': 1,
+        'Ghost': 1,
+        'Steel': .5,
+        'Fire': .5,
+        'Water': .5,
+        'Grass': 2,
         'Electric': 1,
-        'Psychic': .5,
-        'Ice': 2,
-        'Dragon': 1,
-        'Dark': 2,
-        'Fairy': .5
-    }
+        'Psychic': 1,
+        'Ice': .5,
+        'Dragon': 2,
+        'Dark': 1,
+        'Fairy': 1
+    },
     'Dragon': {
-        'Normal': 2,
+        'Normal': 1,
         'Fighting': 1,
-        'Flying': .5,
-        'Poison': .5,
+        'Flying': 1,
+        'Poison': 1,
         'Ground': 1,
-        'Rock': 2,
-        'Bug': .5,
-        'Ghost': 0,
-        'Steel': 2,
+        'Rock': 1,
+        'Bug': 1,
+        'Ghost': 1,
+        'Steel': .5,
         'Fire': 1,
         'Water': 1,
         'Grass': 1,
         'Electric': 1,
-        'Psychic': .5,
-        'Ice': 2,
-        'Dragon': 1,
-        'Dark': 2,
-        'Fairy': .5
-    }
+        'Psychic': 1,
+        'Ice': 1,
+        'Dragon': 2,
+        'Dark': 1,
+        'Fairy': 0
+    },
     'Dark': {
-        'Normal': 2,
-        'Fighting': 1,
-        'Flying': .5,
-        'Poison': .5,
+        'Normal': 1,
+        'Fighting': .5,
+        'Flying': 1,
+        'Poison': 1,
         'Ground': 1,
-        'Rock': 2,
-        'Bug': .5,
-        'Ghost': 0,
-        'Steel': 2,
+        'Rock': 1,
+        'Bug': 1,
+        'Ghost': 2,
+        'Steel': 1,
         'Fire': 1,
         'Water': 1,
         'Grass': 1,
         'Electric': 1,
-        'Psychic': .5,
-        'Ice': 2,
+        'Psychic': 2,
+        'Ice': 1,
         'Dragon': 1,
-        'Dark': 2,
+        'Dark': .5,
         'Fairy': .5
-    }
+    },
     'Fairy': {
-        'Normal': 2,
-        'Fighting': 1,
-        'Flying': .5,
+        'Normal': 1,
+        'Fighting': 2,
+        'Flying': 1,
         'Poison': .5,
         'Ground': 1,
-        'Rock': 2,
-        'Bug': .5,
-        'Ghost': 0,
-        'Steel': 2,
-        'Fire': 1,
+        'Rock': 1,
+        'Bug': 1,
+        'Ghost': 1,
+        'Steel': .5,
+        'Fire': .5,
         'Water': 1,
         'Grass': 1,
         'Electric': 1,
-        'Psychic': .5,
-        'Ice': 2,
-        'Dragon': 1,
+        'Psychic': 1,
+        'Ice': 1,
+        'Dragon': 2,
         'Dark': 2,
-        'Fairy': .5
+        'Fairy': 1
     }
 }
